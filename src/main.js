ce0a3176212260828bfb3113bfbc143b9f1190ae
Víctor Sabido <vsabidorubio@gmail.com>
2021-05-27 17:54:37 +0200

    Vuex init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import notesStore from './store/notes'
 
-createApp(App).mount('#app')
+
+
+createApp(App)
+    .use(notesStore)
+    .mount('#app')
